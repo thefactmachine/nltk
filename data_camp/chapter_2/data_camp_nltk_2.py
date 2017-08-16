a0acebeb080b252546be4dfaf2b6eefe7f1c4d55
thefactmachine <mark.hatcher@thefactmachine>
2017-08-16 20:58:34 +1000

    data camp chapter 2

--- data_camp/chapter_2/data_camp_nltk_2.py
+++ data_camp/chapter_2/data_camp_nltk_2.py
@@ -31,6 +31,7 @@ counter_tokens.most_common(5)
 # examples: tokenisation, lower_casing words, lemmatization / stemming
 # removing stop words, punctuation, unwanted tokens
 # plural nouns singular
+# try different results
  
 # Example:
 # input text: Cats, dogs and birds are common pets. So are fish.
@@ -46,4 +47,72 @@ cat_tokens = [w for w in word_tokenize(str_cat.lower()) if w.isalpha()]
 no_stops = [t for t in cat_tokens if t not in stopwords.words('english')]
  
 Counter(no_stops).most_common(2)
+
+str_path = '/Volumes/ORANGE/Documents/Projects/BigData/Skills/Python/NLTK-2.0/data_camp/chapter_2/article.txt'
+f = open(str_path, 'r')
+article = f.read()
+
+
+from collections import Counter
+
+tokens = word_tokenize(article)
+
+# Convert the tokens into lowercase: lower_tokens
+lower_tokens = [t.lower() for t in tokens]
+
+lower_tokens_no_stops = [t for t in lower_tokens if t not in stopwords.words('english')]
+
+# Create a Counter with the lowercase tokens: bow_simple
+bow_simple = Counter(lower_tokens_no_stops)
+
+# Print the 10 most common tokens
+print(bow_simple.most_common(10))
+
+# =====================================================================
+# =====================================================================
+# =====================================================================
+# Simple Text Preprocess =============================================
+# =====================================================================
+
+# Import WordNetLemmatizer
+from nltk.stem import WordNetLemmatizer
+
+
+# Retain alphabetic words: alpha_only  --- so this gets rid of all sorts of things
+# -- like punctuation and and dates...
+alpha_only = [t for t in lower_tokens_no_stops if t.isalpha()]
+
+# Instantiate the WordNetLemmatizer
+wordnet_lemmatizer = WordNetLemmatizer()
+
+# Lemmatize all tokens into a new list: lemmatized
+lemmatized = [wordnet_lemmatizer.lemmatize(t) for t in alpha_only]
+
+# Create the bag-of-words: bow
+bow = Counter(lemmatized)
+
+# Print the 10 most common tokens
+print(bow.most_common(10))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
